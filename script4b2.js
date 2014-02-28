bea2c5569642abba1a1dcf2c8edb7fd8049d6e15
Sharanya Haridas <sharanya0211@gmail.com>
2014-02-28 14:20:02 -0800

    with document ready function

--- script4b2.js
+++ script4b2.js
@@ -1,3 +1,15 @@
 /**
  * @author Sharanya Haridas
  */
+function pageloaded(){
+	console.log("my document is loaded");
+}
+
+
+
+
+
+
+$document.ready(pageloaded);
+
+console.log("script4b2 js file is loaded");
