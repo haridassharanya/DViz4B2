3ba35eeda3ad203aa6eff3d58c4aa6c4f5efbc33
Sharanya Haridas <sharanya0211@gmail.com>
2014-02-28 16:15:27 -0800

    commiting draft js

--- script4b2.js
+++ script4b2.js
@@ -1,15 +1,35 @@
 /**
  * @author Sharanya Haridas
  */
-function pageloaded(){
-	console.log("my document is loaded");
+function addJSONToPage(jsonData){
+
+	console.log(json4b2.json);
+ var mydataDiv= $("div");
+ mydataDiv.html(jsonData);
+ $("#contentcreator").append("mydataDiv");
+}
+
+function pageLoaded(){
+
+	// jquery div
+	var myjQDiv = $ ("<div>");
+	$myjQDiv.html("This is my jquery div");
+	$("#contentcontainer").append(myjQDiv);
+	
+	//first parameter comment
+	//second
+	//third
+	
+	console.log("My document is loaded");
+	
+	$.get("json4b2.json", addJSONToPage, "json");
+
+	
 }
 
 
+$(document).ready(pageLoaded);
 
 
-
-
-$document.ready(pageloaded);
 
 console.log("script4b2 js file is loaded");
